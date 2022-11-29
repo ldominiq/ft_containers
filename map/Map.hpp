0491c1502615f6523a64335304fb3cc359653dfd
Lucas Dominique <lucasdominique99@gmail.com>
2022-11-29 12:09:26 +0100

    First commit

---
+++ map/Map.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Lucas on 29-Nov-22.
+//
+
+#ifndef FT_CONTAINERS_MAP_HPP
+#define FT_CONTAINERS_MAP_HPP
+
+#endif //FT_CONTAINERS_MAP_HPP
---
+++ vector/Vector.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Lucas on 29-Nov-22.
+//
+
+#ifndef FT_CONTAINERS_VECTOR_HPP
+#define FT_CONTAINERS_VECTOR_HPP
+
+#endif //FT_CONTAINERS_VECTOR_HPP
