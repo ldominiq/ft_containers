5f642e3b96f0a1bb659a4ae1b01ce8d35b340bdf
Lucas Dominique <lucasdominique99@gmail.com>
2022-12-01 14:46:54 +0100

    WIP: Implemented iterator_traits

---
+++ iterator.hpp
@@ -0,0 +1,91 @@
+//
+// Created by Lucas on 01-Dec-22.
+//
+
+#ifndef FT_ITERATOR_HPP
+#define FT_ITERATOR_HPP
+
+
+#include <cstddef>
+
+namespace ft {
+
+    /* ===========================================================================================================
+     * ITERATOR CATEGORIES
+     * =========================================================================================================*/
+
+    /* Empty class to identify the category of an iterator as an input iterator */
+    class   input_iterator_tag {};
+    /* Empty class to identify the category of an iterator as an output iterator */
+    class   output_iterator_tag {};
+    /* Empty class to identify the category of an iterator as a forward iterator */
+    class   forward_iterator_tag {};
+    /* Empty class to identify the category of an iterator as a bidirectional iterator */
+    class   bidirectional_iterator_tag {};
+    /* Empty class to identify the category of an iterator as a random-access iterator */
+    class   random_access_iterator_tag {};
+
+
+    /* ===========================================================================================================
+     * ITERATOR
+     * =========================================================================================================*/
+
+    /**
+     * This is a base class template that can be used to derive iterator classes from it.
+     * It is not an iterator class and does not provide any of the functionality an iterator is expected to have.
+     * @tparam Category Category to which the iterator belongs to. It must be one of the following iterator tags
+     * @tparam T Type of elements pointed by the iterator.
+     * @tparam Distance Type to represent the difference between two iterators.
+     * @tparam Pointer Type to represent a pointer to an element pointed by the iterator.
+     * @tparam Reference Type to represent a reference to an element pointed by the iterator.
+     */
+    template <class Category, class T, class Distance = std::ptrdiff_t, class Pointer = T*, class Reference = T&>
+    struct iterator {
+    public:
+        typedef Category    iterator_category;
+        typedef T           value_type;
+        typedef Distance    difference_type;
+        typedef Pointer     pointer;
+        typedef Reference   reference;
+    };
+
+
+    /* ===========================================================================================================
+     * ITERATOR_TRAITS
+     * =========================================================================================================*/
+
+    /**
+     * Traits class defining properties of iterators.
+     * Standard algorithms determine certain properties of the iterators passed to them and the range
+     * they represent by using the members of the corresponding iterator_traits instantiation.
+     * @tparam Iterator ft::iterator
+     */
+    template< class Iterator >
+    struct iterator_traits {
+        typedef typename Iterator::difference_type      difference_type;
+        typedef typename Iterator::value_type           value_type;
+        typedef typename Iterator::pointer              pointer;
+        typedef typename Iterator::reference            reference;
+        typedef typename Iterator::iterator_category    iterator_category;
+    };
+
+    template< class T >
+    struct iterator_traits<T*> {
+        typedef ptrdiff_t                       difference_type;
+        typedef T                               value_type;
+        typedef T*                              pointer;
+        typedef T&                              reference;
+        typedef ft::random_access_iterator_tag  iterator_category;
+    };
+
+    template< class T >
+    struct iterator_traits<const T*> {
+        typedef ptrdiff_t                       difference_type;
+        typedef T                               value_type;
+        typedef const T*                        pointer;
+        typedef const T&                        reference;
+        typedef ft::random_access_iterator_tag  iterator_category;
+    };
+}
+
+#endif //FT_ITERATOR_HPP
