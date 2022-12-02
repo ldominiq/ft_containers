f74daef5a4b85484da1d13d84af788bf5df72ec1
Lucas Dominique <lucasdominique99@gmail.com>
2022-12-02 16:18:43 +0100

    WIP: Implemented equal

--- algorithm.hpp
+++ algorithm.hpp
@@ -5,11 +5,17 @@
 #ifndef FT_ALGORITHM_HPP
 #define FT_ALGORITHM_HPP
 
-//TODO: Implement equal
 namespace ft {
 
     template <class InputIterator1, class InputIterator2>
-    bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2);
+    bool equal (InputIterator1 first1, InputIterator1 last1, InputIterator2 first2) {
+        for (; first1 != last1; ++first1, ++first2) {
+            if (*first1 != *first2) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 #endif //FT_ALGORITHM_HPP
