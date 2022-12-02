10bee3fad3a7d508fe17e6048509604b89defb67
Lucas Dominique <lucasdominique99@gmail.com>
2022-12-02 16:19:11 +0100

    WIP: Implemented enable_if and is_integral

--- type_traits.hpp
+++ type_traits.hpp
@@ -5,15 +5,96 @@
 #ifndef FT_TYPE_TRAITS_HPP
 #define FT_TYPE_TRAITS_HPP
 
-//TODO: Implement enable_if
-//TODO: Implement is_integral
 namespace ft {
 
-    template <bool Cond, class T = void>
-    struct enable_if;
+    /* ===========================================================================================================
+     * ENABLE_IF
+     * =========================================================================================================*/
 
-    template <class T>
-    struct is_integral;
+    template <bool Cond, class T = void>
+    struct enable_if {};
+
+    template<class T>
+    struct enable_if<true, T> {
+        typedef T type;
+    };
+
+
+    /* ===========================================================================================================
+     * IS_INTEGRAL
+     * =========================================================================================================*/
+
+    template<class T>
+    class is_integral {
+    public:
+        static const bool value = false;
+    };
+
+    template<>
+    class is_integral<int> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<bool> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<char> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<wchar_t> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<signed char> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<short int> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<long int> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<unsigned char> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<unsigned short int> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<unsigned int> {
+    public:
+        static const bool value = true;
+    };
+
+    template<>
+    class is_integral<unsigned long int> {
+    public:
+        static const bool value = true;
+    };
 }
 
 #endif //FT_TYPE_TRAITS_HPP
