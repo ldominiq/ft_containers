b0f64beded601e8723e1b14db3af19004462ac93
Lucas Dominique <lucasdominique99@gmail.com>
2022-12-05 19:54:16 +0100

    WIP: Fixed equal

--- algorithm.hpp
+++ algorithm.hpp
@@ -17,6 +17,16 @@ namespace ft {
         return true;
     }
 
+    template<class InputIt1, class InputIt2, class BinaryPredicate>
+    bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p) {
+        for (; first1 != last1; ++first1, ++first2) {
+            if (!p(*first1, *first2)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     template<class InputIt1, class InputIt2>
     bool lexicographical_compare(InputIt1 first1, InputIt1 last1, InputIt2 first2, InputIt2 last2) {
         for (; (first1 != last1) && (first2 != last2); ++first1, (void) ++first2)
